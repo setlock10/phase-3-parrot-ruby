c254f798c4f96c30d425053a518044d409530a85
John J Setlock II <johnsetlock@gmail.com>
2022-08-28 11:19:06 -0500

    commit tree 7b4302aadbcad705222f745508a9df72ca0821c8

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+require 'pry'
+
+def parrot(phrase="Squawk!")
+   
+        puts phrase
+        return phrase
+
+
+    
+end
+
+binding.pry
